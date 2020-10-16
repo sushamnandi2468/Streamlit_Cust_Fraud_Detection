bf7ed5655c11ec7137166333805bdf7cb892a6c2
Susham Nandi <sushamnandi2468@gmail.com>
2020-10-16 16:42:44 +0530

    error update
    error update

--- setup.sh
+++ setup.sh
@@ -2,9 +2,11 @@ mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
 email = \"sushamnandi2468@gmail.com\"\n\
-" > ~/.streamlit/credentials.toml
+" > ~/.streamlit/credentials.toml 
+
 echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
 port = $PORT\n\
+" > ~/.streamlit/config.toml
